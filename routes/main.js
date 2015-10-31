30a831ac29e2b43359df01e2ed7ad9e3e956e655
migue <miguelisaac1985@hotmail.com>
2015-10-30 22:21:41 -0300

    ABM y algo de estilo

--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,61 @@ var Persons = require('../models/persons.js');
 
 app.get('/list', function(req, res){
 	Persons.find({}, function(err, docs){
-		res.json(docs);
-		console.log(docs);
+		res.render('list', {title: 'List', persons: docs});
 	});
+});
+
+app.get('/new', function(req, res){
+	res.render('new', {title: 'New'});
+});
+
+app.post('/new', function(req, res){
+    res.render('new', { title: 'New'});
+    console.log(req.body);
+    var p = new Persons({ name: req.body.name, age: req.body.age });
+    p.save(function(err, doc){
+        if(!err){
+            res.redirect('/list');
+        } else {
+            res.end(err);    
+        }    
+    });
+});
+
+app.get('/delete/:id', function(req, res){
+    Persons.remove({ _id: req.params.id }, function(err, doc){
+        if(!err){
+            res.redirect('/list');
+        } else {
+            res.end(err);    
+        }    
+    });
+});
+
+app.get('/edit/:id', function(req, res){
+    Persons.findOne({ _id: req.params.id }, function(err, doc){
+        if(!err){
+            res.render('edit', { title: 'Edit', person: doc});
+        } else {
+            res.end(err);    
+        }    
+    });
+});
+
+app.post('/edit/:id', function(req, res){
+    Persons.findOne({ _id: req.params.id }, function(err, doc){
+        if(!err){
+            doc.name = req.body.name; 
+            doc.age = req.body.age;
+            doc.save(function(err, doc){
+                if(!err){
+                    res.redirect('/list');
+                } else {
+                    res.end(err);    
+                }    
+            }); 
+        } else {
+            res.end(err);    
+        }    
+    });
 });
